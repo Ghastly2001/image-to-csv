b595f47a0bff2077bfa337b42ee43f36db36f096
Ghastly2001 <divyanshverma18may@gmail.com>
2024-10-10 12:44:28 +0530

    express-server

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Testing");
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
